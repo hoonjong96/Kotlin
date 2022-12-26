bce86c6fe898e5912bda0a4295a2cd90c449a8d0
Jongdroid <qkr7627@gmail.com>
2022-12-26 23:02:38 +0900

    Study: Method

--- src/main/kotlin/Method.kt
+++ src/main/kotlin/Method.kt
@@ -17,3 +17,16 @@ fun helloWorld(){
 fun add(a: Int, b: Int) : Int {
     return a + b
 }
+
+// 코틀린만의 매력!! Nullable vs Nonnull
+// NPE: Null Pointer Exception 자바를 하면서 정말 많이 만나는 나쁜 뇨속중 하나이다
+// 근데 또 이놈이 자바에서 컴파일 시점에 잡히는게 아니라 런타임에 잡혀서 좀 나쁘다 !
+// 근데 코틀린은 컴파일 시점에 잡아줄게 대신 ? 좀 써줘 .
+fun nullcheck() {
+
+    // 이렇게 ? 가 없으면 Nonnull 타입으로, null이 있으면 안되는 타입이다.
+    var name: String = "Jongdroid"
+
+    // 이렇게 사용해야 nullable 타입이 된다.
+    var nullname: String? = null
+}

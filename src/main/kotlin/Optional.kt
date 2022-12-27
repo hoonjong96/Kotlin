99d4b62405902540962f525bef4bee802d8938f6
Jongdroid <qkr7627@gmail.com>
2022-12-27 18:21:36 +0900

    Study: Optional

--- src/main/kotlin/Optional.kt
+++ src/main/kotlin/Optional.kt
@@ -1,4 +1,6 @@
 fun main(array: Array<String>) {
+    // 옵셔널 -> 노션에 정리 필요
+
 
     // 다른 언어에서는 다음과 같이 값을 초기화 하지 않아서 다음과 같이 null을 포함시킬 수 있지만 코틀린은 이를 불허한다.
     // 다음과 같은 코드는 에러를 만드는 코드다.
@@ -21,16 +23,37 @@ fun main(array: Array<String>) {
     }?: "가보자잇"
     println("some Value: $someValue")
 
-    val a = "Kotlin"
-    val b: String? = "하윙"
+    // * 중요한 개념 **
 
-//    println(b?)
-    println("${a.length}")
+    var userName: String? = null
 
-    var upperString: String? = null
+    // var usernameUpperCase = userName.toUpperCase()
+    // 이러면 에러가 발생한다. -> userName이 null인지 null이 아닌지 몰라서 예를 사용할 수 있는지 모르겠어.
 
-//    println("${upperString.toUpperCase()}")
+    // 이런 경우는 물음표를 붙이면 된다.
+    // ? 를 붙이는 의미는 만약 userName이 null이 아니면 함수를 실행하고, 그게 아니면 userName.toUpperCase() 자체를 null로 반환한다.
+    // 스위프트의 옵셔널 바인딩 느낌
+    var usernameUpperCase = userName?.toUpperCase()
+    println(usernameUpperCase)
 
-    var testString = upperString?: "해윙"
-    println("testString: $testString")
+    // ?: 엘비스 연산자 -> 엘비스 프레슬리 헤어 닮아서 엘비스 ㅋㅋ
+    // 위에서는 바로 null을 반환했다면, 이 친구는 디폴트 값을 설정할 수 있다.
+
+    // 값이 있어 없어? 만약 없으면 (null) 이걸로 해줭
+    // userName이 null 이면 -> 아직 값이 없는 것 같아요. (디폴트 값)
+    // userName에 값이 있으면 그 값을 출력한다.
+    var displayUserName = "Hello" + (userName?: "아직 값이 없는 것 같아요")
+    println(displayUserName)
+
+    if (userName != null) {
+        println("널이 아닙니답쇼")
+    } else {
+        println("널이라서 기본 값 지정할래요")
+    }
+
+    // 또 많이 사용하는 녀석으로
+    // !! 가 있다. 개발자가 컴파일 시점에 "이 녀석은 하늘이 무너져도 절대 null이 아닌 걸 인지하고 있을때 컴파일러에게 !! 을 붙여서 알려준다."
+
+
+
 }
